2461b246fd4c6d8a43cb5ca9a2e4ce132339608f
ptadros <peter.aboalim@gmail.com>
2020-12-31 22:31:11 +0200

    move whiskeys controller under api namespace

--- app/controllers/api/whiskeys_controller.rb
+++ app/controllers/api/whiskeys_controller.rb
@@ -1,4 +1,4 @@
-class WhiskeysController < ApplicationController
+class Api::WhiskeysController < ApplicationController
   def index
     whiskeys = Whiskey.order(:title).all
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :whiskeys, only: [:index, :create]
+  end
   root 'home#index'
-
-  resources :whiskeys, only: [:index, :create]
-
   get '/*path' => 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
